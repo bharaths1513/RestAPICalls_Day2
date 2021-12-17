3088c58f6bcd439dae5e33b51eb597d61ace8c04
bharaths1513 <bharaths.1311@gmail.com>
2021-12-17 21:36:21 +0530

    added: UC5 passing firstname as path variable and lastname as Query parameter

--- src/main/java/com/example/RestAPIController.java
+++ src/main/java/com/example/RestAPIController.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,54 +12,66 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
 
-
-
 @RestController
 @RequestMapping("/hello")
 public class RestAPIController {
-	
+
 	/**
 	 * Call Get method to return the text
+	 * 
 	 * @return
 	 */
-	@RequestMapping(value = {"","/","/home"})
+	@RequestMapping(value = { "", "/", "/home" })
 	public String index() {
 		return "Hello from Bridgelabz...";
 	}
-	
-	
+
 	/**
 	 * Calling Get Request Method and pass name as query parameter
 	 * localhost:8080/hello/query?name=Mark
+	 * 
 	 * @param name
 	 * @return
 	 */
-	@RequestMapping(value= {"/query"} , method = RequestMethod.GET)
-	public String sayHello(@RequestParam(value= "name") String name){
-		return "Hello" +" "+ name + " "+ "from Bridgelabz";
+	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello" + " " + name + " " + "from Bridgelabz";
 	}
-	
-	
+
 	/**
 	 * Calling Get Request method and pass name as path variable
 	 * localhost:8080/hello/param/Mark -w "/n"
+	 * 
 	 * @param name
 	 * @return
 	 */
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
-		return "Hello"+" " + name +" "+ "from Bridgelabz"; 
+		return "Hello" + " " + name + " " + "from Bridgelabz";
 	}
-	
-	
+
 	/**
 	 * Call Post Request Method and pass first name and last name in Body
 	 * localhost:8080/hello/post -w "/n"
+	 * 
 	 * @param user
 	 * @return
 	 */
 	@PostMapping("/post")
 	public String postData(@RequestBody User user) {
-		return "Hello " + user.getFirstName() + " " + user.getLastName();	
+		return "Hello " + user.getFirstName() + " " + user.getLastName();
+	}
+
+	/**
+	 * use Put Request Method and pass first name as path variable and last name as
+	 * Query Parameter localhost:8080/hello/Mark/?lastname=Taylor -w "/n"
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @return
+	 */
+	@PutMapping("/put/{firstName}")
+	public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + " From bridgeLabz";
 	}
 }
