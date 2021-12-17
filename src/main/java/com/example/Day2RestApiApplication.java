0fbe641c4ffd15814cd857d9fc0f8d69b3adec61
bharaths1513 <bharaths.1311@gmail.com>
2021-12-17 20:52:19 +0530

    added: Rest Controller

---
+++ src/main/java/com/example/Day2RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2RestApiApplication.class, args);
+	}
+
+}
