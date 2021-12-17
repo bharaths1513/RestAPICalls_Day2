9a0bb1185997d03b3c898c66fbb198f94bf8f1a8
bharaths1513 <bharaths.1311@gmail.com>
2021-12-17 20:56:05 +0530

    added: UC1_Creating Rest Controller

--- src/main/java/com/example/RestAPIController.java
+++ src/main/java/com/example/RestAPIController.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,5 +17,16 @@ public class RestAPIController {
 	@RequestMapping(value = {"","/","/home"})
 	public String index() {
 		return "Hello from Bridgelabz...";
+	}
+	
+	/**
+	 * Calling Get Request Method and pass name as query parameter
+	 * localhost:8080/hello/query?name=Mark
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping(value= {"/query"} , method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value= "name") String name){
+		return "Hello" +" "+ name + " "+ "from Bridgelabz";
 	}
 }
