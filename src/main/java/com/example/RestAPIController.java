7d03df28abc804f8d751049462d6036e77e26a00
bharaths1513 <bharaths.1311@gmail.com>
2021-12-17 21:30:18 +0530

    added: UC4 Passing firstname and lastname in the body

--- src/main/java/com/example/RestAPIController.java
+++ src/main/java/com/example/RestAPIController.java
@@ -2,16 +2,21 @@ package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
 
 
 
 @RestController
 @RequestMapping("/hello")
 public class RestAPIController {
+	
 	/**
 	 * Call Get method to return the text
 	 * @return
@@ -33,6 +38,7 @@ public class RestAPIController {
 		return "Hello" +" "+ name + " "+ "from Bridgelabz";
 	}
 	
+	
 	/**
 	 * Calling Get Request method and pass name as path variable
 	 * localhost:8080/hello/param/Mark -w "/n"
@@ -42,5 +48,17 @@ public class RestAPIController {
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello"+" " + name +" "+ "from Bridgelabz"; 
+	}
+	
+	
+	/**
+	 * Call Post Request Method and pass first name and last name in Body
+	 * localhost:8080/hello/post -w "/n"
+	 * @param user
+	 * @return
+	 */
+	@PostMapping("/post")
+	public String postData(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName();	
 	}
 }
